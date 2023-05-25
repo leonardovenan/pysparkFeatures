44ed5decfdaced239261a6946b728ad419019ff2
leonardovenan <leonardovenancio12@gmail.com>
2023-05-25 11:15:18 -0300

    Função utilizada no databricks para normalização de dataframes, geralmente quando recebo arquivos .json

---
+++ normalization.py
@@ -0,0 +1,25 @@
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+def normalization(df):
+  complex_fields = dict([(field.name, field.dataType)
+                         for field in df.schema.fields
+                         if(type(field.dataType) == ArrayType or type(field.dataType) == StructType)
+                         ])
+  while len(complex_fields) != 0:
+    col_name = list(complex_fields.keys())[0]
+    print(f'Processing: {col_name} Type: {str(type(complex_fields[col_name]))}')
+# Se for StructType então vamos converter todos os sub elementos em colunas        
+    if(type(complex_fields[col_name]) == StructType):
+      expanded = [col(col_name+'.'+k).alias(col_name+'_'+k) for k in [n.name for n in complex_fields[col_name]]]
+      df = df.select("*", *expanded).drop(col_name)
+
+    elif(type(complex_fields[col_name]) == ArrayType):
+      df = df.withColumn(col_name, explode_outer(col_name))
+
+      complex_fields = dict([(field.name, field.dataType)
+                             for field in df.schema.fields
+                             if(type(field.dataType) == ArrayType or type(field.dataType) == StructType)
+                             ])
+      
+  return df
