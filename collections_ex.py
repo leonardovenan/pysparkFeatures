59f8173f09b2cbd8dd6c66d4d0c1b104e8b8ab1f
leonardovenan <leonardovenancio12@gmail.com>
2023-05-29 17:27:49 -0300

    método de verificação 'most_common' em uma lista

--- collections_ex.py
+++ collections_ex.py
@@ -1,12 +1,13 @@
 from collections import namedtuple
 from collections import defaultdict
+from collections import Counter
 
 Person = namedtuple('Person', ['name', 'age', 'job'])
 
 person1 = Person('Leonard', 29, 'Data Engineer')
 
 print(f'Name: {person1.name}\nAge: {person1.age}\nJob: {person1.job}')
-
+#-----------------------------------------------------------------------------------------
 word_count = defaultdict(int)
 
 words = ['um', 'dois', 'três', 'um', 'quatro', 'um', 'cinco', 'seis', 'seis', 'três']
@@ -14,3 +15,9 @@ for word in words:
     word_count[word] += 1
 
 print(word_count)
+
+#-----------------------------------------------------------------------------------------
+compras = ['mamao', 'água', 'tomate', 'tomate', 'desodorante', 'desodorante', 'água', 'água']
+compras_counter = Counter(compras)
+
+print(compras_counter.most_common(3))
