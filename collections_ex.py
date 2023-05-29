a59e7b401fac564d60b56401f1c0b498f63fba0a
leonardovenan <leonardovenancio12@gmail.com>
2023-05-29 17:14:14 -0300

    Adição de método de contagem de repetições em uma lista

--- collections_ex.py
+++ collections_ex.py
@@ -7,3 +7,10 @@ person1 = Person('Leonard', 29, 'Data Engineer')
 
 print(f'Name: {person1.name}\nAge: {person1.age}\nJob: {person1.job}')
 
+word_count = defaultdict(int)
+
+words = ['um', 'dois', 'três', 'um', 'quatro', 'um', 'cinco', 'seis', 'seis', 'três']
+for word in words:
+    word_count[word] += 1
+
+print(word_count)
