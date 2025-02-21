e7f936396185b4a293abf11cc975c535436ba78b
Leonardo Venancio Correia <leonardovenancio12@gmail.com>
2025-02-21 11:27:36 -0300

    Unity teste Refactory

--- unity_test.py
+++ unity_test.py
@@ -1,21 +1,16 @@
 import pytest
 from pyspark.sql import functions as F
 
-def verificar_soma_valores_zero(df):
-    valores_lista = [row['valor__base'] for row in df.collect()]
-    return all(valor == 0 for valor in valores_lista), valores_lista
-
-def test_valores_zero():
-    df_unity_test = spark.sql("""
+df_unity_test = spark.sql("""
     with repetion as (
-        select ff___contrato, count(0) from dbstage.tb_storm_relatorio_comissao_banco
+        select ff___contrato, count(0) from dbstage.tb_relatorio_comissao
         group by ff___contrato
         having count(0) > 1
     ), sum_groupby as (
       select
         a.ff___contrato,
         sum(cast(REPLACE(REPLACE(valor__base, '.', ''), ',', '.') as float)) as valor__base
-      from dbstage.tb_storm_relatorio_comissao_banco a
+      from dbstage.tb_relatorio_comissao a
       inner join repetion b
       on a.ff___contrato = b.ff___contrato
       group by a.ff___contrato
@@ -23,12 +18,17 @@ def test_valores_zero():
     select * from sum_groupby
     """)
 
-    total_duplicados = df_unity_test.count()
+def verificar_soma_valores_zero(df):
+    valores_lista = [row['valor__base'] for row in df.collect()]
+    return all(valor == 0 for valor in valores_lista), valores_lista
+
+def test_valores_zero(df_spark):
+    total_duplicados = df_spark.count()
     if total_duplicados == 0:
         print("✅ Nenhum contrato duplicado encontrado. Teste ignorado.")
         return
     
-    todos_zero, valores_lista = verificar_soma_valores_zero(df_unity_test)
+    todos_zero, valores_lista = verificar_soma_valores_zero(df_spark)
     
     if todos_zero:
         print("✅ Teste passou com sucesso! Todas as somas de valor__base são 0.")
@@ -37,6 +37,6 @@ def test_valores_zero():
         discord_alarm_comissao(mensagem_erro)
         pytest.fail(mensagem_erro) 
 
-    df_unity_test.show(truncate=False)
+    df_spark.show(truncate=False)
 
-test_valores_zero()
+test_valores_zero(df_unity_test)
